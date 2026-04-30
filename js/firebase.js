1b3416271ef92ec02b26cc7a17669193df8e6baa
SwisaAssociates <saalimasad@shaheenpublicschool.in>
2026-04-30 17:10:33 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,8 +1,7 @@
-// Firebase setup
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+/* 🔥 Firebase Config (SWISA SYSTEM) */
 const firebaseConfig = {
   apiKey: "AIzaSyC1KD3II4xpP2V1MY4M2-EelW4pvUBsacU",
   authDomain: "swisa-associates1.firebaseapp.com",
@@ -12,5 +11,8 @@ const firebaseConfig = {
   appId: "1:730305882663:web:bfc45f64ee34b78836fc02"
 };
 
+/* 🚀 Initialize App Safely */
 const app = initializeApp(firebaseConfig);
+
+/* 📦 Firestore Database Export */
 export const db = getFirestore(app);
